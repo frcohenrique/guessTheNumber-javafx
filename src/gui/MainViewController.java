79dfa2631d4c100bd3bb3813007809c833cb9549
Henrique Franco <henrique.frco@gmail.com>
2021-07-10 20:38:09 -0300

    Added main game functions

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import gui.utils.Constraints;
@@ -12,6 +13,9 @@ import javafx.scene.control.TextField;
 
 public class MainViewController implements Initializable {
 
+	private Integer randomNumber;
+	private Integer tries = 10;
+	
 	@FXML
 	private TextField txtNumberInput;
 	@FXML
@@ -24,11 +28,27 @@ public class MainViewController implements Initializable {
 	private Label labelAnswer;
 	
 	public void onBtGuessAction() {
+		int numberInput = Integer.parseInt(txtNumberInput.getText());
+		if (numberInput != randomNumber) {
+			tries -= 1;
+			if (tries == 0) {
+				labelAnswer.setText("You lost! The answer was " + String.valueOf(randomNumber));
+				disableComponents();
+			}
+			else {
+				labelAnswer.setText("Incorrect. Try again!");
+				labelTriesLeft.setText(String.valueOf(tries));
+			}
+		}
+		else {
+			labelAnswer.setText("You got it! The number was" + String.valueOf(randomNumber));
+			disableComponents();
+		}
 		System.out.println("onBtGuessAction");
 	}
 	
 	public void onBtRestartAction() {
-		System.out.println("onBtRestartAction");
+		resetGame();
 	}
 	
 	@Override
@@ -42,10 +62,31 @@ public class MainViewController implements Initializable {
 		
 		Constraints.setTextFieldMax(txtNumberInput, 3);
 		Constraints.setTextFieldToInteger(txtNumberInput);
-		
+		labelTriesLeft.setText(String.valueOf(tries));
+		createRandomNumber();
 		
 	}
+	
+	private void resetGame() {
+		labelTriesLeft.setText("");
+		labelAnswer.setText("");
+		this.tries = 5;
+		btGuess.setVisible(true);
+		txtNumberInput.setEditable(true);
+		txtNumberInput.setText("");
+		createRandomNumber();
+	}
+	
+	private void disableComponents() {
+		btGuess.setVisible(false);
+		txtNumberInput.setEditable(false);
+	}
 
+	public void createRandomNumber() {
+		Random random = new Random();
+		int number = random.nextInt(5);
+		randomNumber = number;
+	}
 	
 	
 }

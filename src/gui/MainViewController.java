fd936b861b9e87f8c3f8d813ed16e684dda008e2
Henrique Franco <henrique.frco@gmail.com>
2021-07-10 20:56:53 -0300

    Added Alerts class and did improvements to the game.

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -4,18 +4,20 @@ import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
 
+import gui.utils.Alerts;
 import gui.utils.Constraints;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class MainViewController implements Initializable {
 
 	private Integer randomNumber;
-	private Integer tries = 10;
-	
+	private Integer tries = 15;
+
 	@FXML
 	private TextField txtNumberInput;
 	@FXML
@@ -26,67 +28,74 @@ public class MainViewController implements Initializable {
 	private Label labelTriesLeft;
 	@FXML
 	private Label labelAnswer;
-	
+
 	public void onBtGuessAction() {
-		int numberInput = Integer.parseInt(txtNumberInput.getText());
-		if (numberInput != randomNumber) {
-			tries -= 1;
-			if (tries == 0) {
-				labelAnswer.setText("You lost! The answer was " + String.valueOf(randomNumber));
-				disableComponents();
-			}
-			else {
-				labelAnswer.setText("Incorrect. Try again!");
-				labelTriesLeft.setText(String.valueOf(tries));
-			}
+		try {
+			guessNumber();
+		}catch(NumberFormatException e) {
+			Alerts.showAlert("Error!", null, "TextField can't be empty!", AlertType.ERROR);
 		}
-		else {
-			labelAnswer.setText("You got it! The number was" + String.valueOf(randomNumber));
-			disableComponents();
-		}
-		System.out.println("onBtGuessAction");
+		
 	}
-	
+
 	public void onBtRestartAction() {
 		resetGame();
 	}
+
 	
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		
-		initializeNodes();
-		
-	}
-	
-	public void initializeNodes() {
-		
-		Constraints.setTextFieldMax(txtNumberInput, 3);
-		Constraints.setTextFieldToInteger(txtNumberInput);
-		labelTriesLeft.setText(String.valueOf(tries));
-		createRandomNumber();
-		
-	}
-	
+
 	private void resetGame() {
 		labelTriesLeft.setText("");
 		labelAnswer.setText("");
-		this.tries = 5;
+		this.tries = 15;
+		labelTriesLeft.setText(String.valueOf(tries));
 		btGuess.setVisible(true);
 		txtNumberInput.setEditable(true);
 		txtNumberInput.setText("");
 		createRandomNumber();
 	}
 	
+	private void guessNumber() {
+		int numberInput = Integer.parseInt(txtNumberInput.getText());
+		if (numberInput != randomNumber) {
+			tries -= 1;
+			if (tries == 0) {
+				labelTriesLeft.setText("0");
+				labelAnswer.setText("You lost! The answer was " + String.valueOf(randomNumber));
+				disableComponents();
+			} else {
+				labelAnswer.setText("Incorrect. Try again!");
+				labelTriesLeft.setText(String.valueOf(tries));
+			}
+		} else {
+			labelAnswer.setText("You got it! The number was" + String.valueOf(randomNumber));
+			disableComponents();
+		}
+	}
+
 	private void disableComponents() {
 		btGuess.setVisible(false);
 		txtNumberInput.setEditable(false);
 	}
 
-	public void createRandomNumber() {
+	private void createRandomNumber() {
 		Random random = new Random();
-		int number = random.nextInt(5);
+		int number = random.nextInt(51);
 		randomNumber = number;
 	}
-	
-	
+
+	public void initializeNodes() {
+
+		Constraints.setTextFieldMax(txtNumberInput, 3);
+		Constraints.setTextFieldToInteger(txtNumberInput);
+		labelTriesLeft.setText(String.valueOf(tries));
+		createRandomNumber();
+
+	}
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+		initializeNodes();
+
+	}
 }
--- src/gui/utils/Constraints.java
+++ src/gui/utils/Constraints.java
@@ -5,19 +5,19 @@ import javafx.scene.control.TextField;
 public class Constraints {
 
 	public static void setTextFieldToInteger(TextField txt) {
-		txt.textProperty().addListener((obs, oldValue, newValue) ->{
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
 			if (newValue != null && !newValue.matches("\\d*")) {
 				txt.setText(oldValue);
 			}
 		});
 	}
-	
+
 	public static void setTextFieldMax(TextField txt, int max) {
-		txt.textProperty().addListener((obs, oldValue, newValue) ->{
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
 			if (newValue != null && newValue.length() > max) {
 				txt.setText(oldValue);
 			}
 		});
 	}
-	
+
 }
